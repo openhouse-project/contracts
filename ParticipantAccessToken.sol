721a4f54ea2614fbdd1fdb4bc52c2b79c61e0adc
Chris Hobcroft <github@chrishobcroft.com>
2021-06-23 19:34:43 +0530

    Removed PAUSER_ROLE
    Remove this role until it is better understood, and required.

--- ParticipantAccessToken.sol
+++ ParticipantAccessToken.sol
@@ -1,22 +1,20 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.1.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.1.0/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@4.1.0/security/Pausable.sol";
-import "@openzeppelin/contracts@4.1.0/access/AccessControl.sol";
-import "@openzeppelin/contracts@4.1.0/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts@4.1.0/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ParticipantAccessToken is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
+contract ParticipantAccessToken is ERC721, ERC721Enumerable, ERC721Burnable, AccessControl {
     using Counters for Counters.Counter;
 
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("Participant Access Token", "PAT") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
     }
 
@@ -26,19 +24,8 @@ contract ParticipantAccessToken is ERC721, ERC721Enumerable, Pausable, AccessCon
         _tokenIdCounter.increment();
     }
 
-    function pause() public {
-        require(hasRole(PAUSER_ROLE, msg.sender));
-        _pause();
-    }
-
-    function unpause() public {
-        require(hasRole(PAUSER_ROLE, msg.sender));
-        _unpause();
-    }
-
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
-        whenNotPaused
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
