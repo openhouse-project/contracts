2451b4e68a9bc925d856fa7b958e1b90c28e02ce
Chris Hobcroft <github@chrishobcroft.com>
2021-06-23 19:47:24 +0530

    Remove license line

--- ParticipantAccessToken.sol
+++ ParticipantAccessToken.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts@4.1.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.1.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.1.0/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.1.0/access/AccessControl.sol";
+import "@openzeppelin/contracts@4.1.0/utils/Counters.sol";
 
 contract ParticipantAccessToken is ERC721, ERC721Enumerable, ERC721Burnable, AccessControl {
     using Counters for Counters.Counter;
