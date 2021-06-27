fcf9ff101fdef030ff11f4e3067982e0e886e859
Andrew Ewing <drew@phenocode.com>
2021-06-26 21:29:19 -0700

    Add listMemberRooms, isRoomPublic

--- OpenHouse.sol
+++ OpenHouse.sol
@@ -12,8 +12,10 @@ contract OpenHouse {
     string[] private rooms;
     
     mapping (string => bool) private _roomsMap;
+    mapping (string => bool) private _roomsPublic;
     
     mapping (string => address[]) private _memberships;
+    mapping (address => string[]) private _reverseMemberships;
     
     mapping (string => mapping (address => bool)) private _membershipsMap;
 
@@ -21,9 +23,10 @@ contract OpenHouse {
      * @dev Create a room, or join it if it already exists.
      * @param name defines the room
      */
-    function addRoom(string calldata name) public {
+    function addRoom(string calldata name, bool isPublic) public {
         if (!_roomsMap[name]){
             _roomsMap[name] = true;
+            _roomsPublic[name] = isPublic;
             rooms.push(name);
         }
         
@@ -31,6 +34,7 @@ contract OpenHouse {
         if (!_membershipsMap[name][sender]) {
             _membershipsMap[name][sender] = true;
             _memberships[name].push(sender);
+            _reverseMemberships[sender].push(name);
         }
     }
     
@@ -39,6 +43,37 @@ contract OpenHouse {
      */
     function listRooms() public view returns (string[] memory) {
         return rooms;
+    }
+    
+    /**
+     * @dev List all rooms for which the sender has membership
+     */
+    function listSenderRooms() public view returns (string[] memory) {
+        address sender = msg.sender;
+        return _reverseMemberships[sender];
+    }
+    
+    /**
+     * @dev Check if the sender is a member of the given room
+     * @param name specifies the room to check membership for
+     */
+    function senderIsInRoom(string calldata name) public view returns (bool) {
+        address sender = msg.sender;
+        return _membershipsMap[name][sender];
+    }
+    
+    /**
+     * @dev Check if a room exists
+     */
+    function roomExists(string calldata name) public view returns (bool) {
+        return _roomsMap[name];
+    }
+    
+    /**
+     * @dev Check if a room is public
+     */
+    function roomIsPublic(string calldata name) public view returns (bool) {
+        return _roomsPublic[name];
     }
     
     /**
