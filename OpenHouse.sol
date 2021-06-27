7522517d5b0d0179d0257fed975465964e699c62
Joe Petrich <petrich89@gmail.com>
2021-06-27 00:54:08 -0400

    Added a few more methods for adding members, setting privacy

--- OpenHouse.sol
+++ OpenHouse.sol
@@ -16,6 +16,18 @@ contract OpenHouse {
     mapping (string => address[]) private _memberships;
     
     mapping (string => mapping (address => bool)) private _membershipsMap;
+    
+    mapping (address => string[]) private _userToRoomsMap;
+    
+    mapping (string => bool) private _roomPrivacyMap;
+    
+    function addUserToRoom(string calldata name, address user) private {
+        if (!_membershipsMap[name][user]){
+            _membershipsMap[name][user] = true;
+            _memberships[name].push(user);
+            _userToRoomsMap[user].push(name);
+        }
+    }
 
     /**
      * @dev Create a room, or join it if it already exists.
@@ -26,11 +38,19 @@ contract OpenHouse {
             _roomsMap[name] = true;
             rooms.push(name);
         }
-        
-        address sender = msg.sender;
-        if (!_membershipsMap[name][sender]) {
-            _membershipsMap[name][sender] = true;
-            _memberships[name].push(sender);
+        addUserToRoom(name, msg.sender);
+    }
+    
+    /**
+     * @dev Add the specified list of members to a room, creating it if it doesn't exist.
+     * @param name of the room
+     * @param members to add to the room
+     */
+    function addRoomWithMembers(string calldata name, address[] calldata members) public {
+        addRoom(name);
+        for (uint i = 0; i < members.length; i++) {
+            address member = members[i];
+            addUserToRoom(name, member);
         }
     }
     
@@ -42,10 +62,37 @@ contract OpenHouse {
     }
     
     /**
-     * @dev Return the list of addresses that belong to a room.
-     * @param name specifies the room to list memberships for.
+     * @dev List the rooms for which the caller is a member.
      */
-    function members(string calldata name) public view returns (address[] memory) {
-        return _memberships[name];
+    function listMyRooms() public view returns (string[] memory) {
+        return _userToRoomsMap[msg.sender];
+    }
+    
+    /**
+     * @dev Return the status of a caller's membership in the given room
+     * @param name of the room
+     */
+    function isMember(string calldata name) public view returns (bool) {
+        return _membershipsMap[name][msg.sender];
+    }
+    
+    /**
+     * @dev Make the requested room private if the user is a member.
+     * @param name of the room to make private.
+     */
+    function makeRoomPrivate(string calldata name) public {
+        if (_membershipsMap[name][msg.sender]) {
+            _roomPrivacyMap[name] = true;
+        }
+    }
+    
+    /**
+     * @dev Make the requested room open if the user is a member.
+     * @param name of the room to make open.
+     */
+    function makeRoomOpen(string calldata name) public {
+        if (_membershipsMap[name][msg.sender]) {
+            _roomPrivacyMap[name] = false;
+        }
     }
 }
